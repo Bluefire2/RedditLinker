d40b1a98f11b3f83b80456dacfcd786dae56f6a9
Bluefire2 <chernyshov.k@gmail.com>
2018-01-07 12:59:38 +0200

    Make it say how many guilds it's in on restart

--- redditlinker.py
+++ redditlinker.py
@@ -27,6 +27,7 @@ async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
+    print('Currently in %d servers' % len(client.servers))
     print('------')
 
 
